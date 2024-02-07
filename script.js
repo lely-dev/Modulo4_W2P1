e683497f035e390e8df837bf93eb0afef1075b50
elisa carboni <elisacarboni@live.com>
2024-02-07 23:57:26 +0100

    html fix

--- script.js
+++ script.js
@@ -11,38 +11,34 @@ function getData(search){
 
 }
 
+let imput = document.getElementById("imput_search");
 
 function showImg(json){
- 
+    
+    let titleContainer = document.getElementById("title_search");
     let imgContainer = document.getElementById("container_picture");
+    // titleContainer.innerHTML = "";
     imgContainer.innerHTML = "";
+
+    titleContainer.innerHTML = imput.value;
+    console.log(titleContainer);
 
     json.photos.forEach(element => {
 
         let imgFind = element.src.medium;
-        // imgContainer.innerHTML += '<div class="card d-flex col-md-3"><img src="'+imgFind+'" class="card-img" alt="..."><div class="card-img-overlay"><h5 class="card-title">Card title</h5></div></div>'
-        imgContainer.innerHTML += '<div class="col-6 col-md-4" id="img_to_show"><img src="'+imgFind+'" alt="img" class="card-img"></div>'
+        // imgContainer.innerHTML += '<div class="col-6 col-md-4 img-fluid" id="img_to_show"  style="width: 18rem;"><img src="'+imgFind+'" alt="img" class="card-img"></div>'
+        imgContainer.innerHTML += '<div class="col-md-3"><div class="card"><div class="card-img"><img src="'+imgFind+'" class="img-fluid"></div><div class="card-img-overlay">Slide 1</div></div></div>'
     });
 }
 
 
+
 // getData("cane");
 
-
-// let dogBtn = document.getElementById("btn_dog");
-// let catBtn = document.getElementById("btn_cat");
 let searchBtn = document.getElementById("btn_search");
 
-// dogBtn.addEventListener("click", () =>{
-//     getData("dog");
-// });
-
-// catBtn.addEventListener("click", () =>{
-//     getData("cat");
-// });
-
 searchBtn.addEventListener("click", () =>{
-    let imput = document.getElementById("imput_search");
+    
     getData(imput.value);
 
 });
